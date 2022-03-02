af6808d0a9bb809ee96931f783b1df6b71a82276
giftedfinger <olakunle.iyiola@yahoo.com>
2022-03-02 12:13:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+const icon = document.querySelectorAll('.icon')
+const sideBar = document.querySelector('.side_bar')
+
+for ( icons of icon){
+    icons.addEventListener('click', () => {
+      
+        sideBar.style.transition="all 2s";
+    sideBar.classList.toggle('sideBar-toggle')
+  
+
+})
+}
+
+let list = document.querySelectorAll('sideBar-item');
+let no = [1,2,3,4,5,6,3,]
+for (let i=0;i<list.length; i++) {
+    list[i].addEventListener('click',()=>{
+if(list[i]%2==0){
+    list.style.border='1px solid #ccc'
+}
+    })
+if([i] % 2===0){
+         console.log(no[i])
+    }
+    
+}
+
+// bottom nav
+let bottomsNav = document.querySelectorAll('.nav-b')
+
+// bottomsNav.forEach( nav =>{
+for(let i =0; i<bottomsNav.length;i++){
+    bottomsNav[i].addEventListener('click',()=>{
+        bottomsNav[i].style.backgroud='red'
+        console.log('yes')
+    })
+}
+
+
+
