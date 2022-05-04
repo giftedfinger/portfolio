2341711230e62b80ef3a7be46c5feb87db20ce6d
giftedfinger <olakunle.iyiola@yahoo.com>
2022-05-04 17:19:18 +0100

    update

--- script.js
+++ script.js
@@ -9,32 +9,86 @@ for ( icons of icon){
   
 
 })
+
 }
 
-let list = document.querySelectorAll('sideBar-item');
-let no = [1,2,3,4,5,6,3,]
-for (let i=0;i<list.length; i++) {
-    list[i].addEventListener('click',()=>{
-if(list[i]%2==0){
-    list.style.border='1px solid #ccc'
+const projects=[{
+    id:1,
+    img:"rtrtyfg",
+    title:'img/bg1.jpg',
+    project:'wewew',
+    bg:'#fb9405'
+
+},
+
+{
+    id:2,
+    img:"img/bg2.jpg",
+    title:'title 1',
+    project:'description',
+bg:'#f0f2f8',
+},
+
+{
+    id:3,
+    img:"img/bg3.png",
+    title:'title 2',
+    project:'wewew',
+    bg:'#192220'
+
+},
+
+{
+    id:4,
+    img:"img/1577481746612.jpg",
+    title:'title 3',
+    project:'im title 2 and ready',
+    bg:'#192220'
+
 }
-    })
-if([i] % 2===0){
-         console.log(no[i])
-    }
+]
+;
+
+
+const img= document.querySelector('.img-div');
+const title= document.querySelector('.project-title');
+const dis= document.querySelector('.project-dis');
+const next= document.querySelector('.btn-next');
+const previous = document.querySelector('.btn-pre')
+
+// 
+let currentProject=3;
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    showProject()
+
     
+})
+
+
+function showProject(){
+
+    const project= projects[currentProject]
+    title.textContent =project.title;
+    dis.textContent = project.project ;
+    img.src= project.img;
 }
 
-// bottom nav
-let bottomsNav = document.querySelectorAll('.nav-b')
-
-// bottomsNav.forEach( nav =>{
-for(let i =0; i<bottomsNav.length;i++){
-    bottomsNav[i].addEventListener('click',()=>{
-        bottomsNav[i].style.backgroud='red'
-        console.log('yes')
-    })
-}
+next.addEventListener('click', ()=>{
+    currentProject++;
+    if(currentProject > projects.length-1){
+        currentProject =0 
+        
+    }
+    showProject()
 
 
+})
 
+previous.addEventListener('click', ()=>{
+    currentProject--;
+    if(currentProject <0 ){
+        currentProject = projects.length-1;
+    }
+showProject()
+})
